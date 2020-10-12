81674f55c58011cadb4c9bf2c387fd5e37254e80
Sarvesh Khandelwal <khandelwalsarvesh8@gmail.com>
2020-10-12 17:07:54 +0530

    Update SideBar.js

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -27,30 +27,35 @@ export class SideBar extends Component {
 }
 
 const Label = styled.a`
-  display: block;
-  padding: 3px 3px;
-  margin: 5px 5px;
+display: block;
+padding: 10px;
+transition: ease 2s;
+background-color: ${(props) =>
+  props.isActiveLink ? "rgb(153, 152, 152)" : "none"};
+z-index: ${(props) => (props.isActiveLink ? "-1" : "none")};
 
-  img {
-    width: 100%;
-    height: auto;
-    display: block;
-    box-shadow: ${(props) =>
-      props.isActiveLink ? "2px 2px 3px 2px rgba(61,8,33,1)" : "none"};
-    z-index: ${(props) => (props.isActiveLink ? "-1" : "none")};
-  }
-  img:hover {
-    box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
-    opacity: 0.6;
-    cursor: pointer;
-  }
+&:hover{
+  background-color:darkgrey;
+  cursor: pointer;
+}
+img {
+  width: 35px;
+  height: 35px;
+  display: block;
+}
+@media (max-width: 900px) {
+  padding: 10px;
+}
 `;
 
 const SideMenu = styled.div`
-  width: 50px;
-  height: 100vh;
-  background-color: #f9cdc4;
-  overflow: hidden;
+width: 60px;
+height: 100vh;
+background-color: lightgrey;
+overflow: hidden;
+@media (max-width: 900px) {
+  width: 80px;
+}
 `;
 
 export default SideBar;
