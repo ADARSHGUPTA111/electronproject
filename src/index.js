0904ab266fc943c311e9c795ca15cc776e1a0d4e
Sarvesh Khandelwal <khandelwalsarvesh8@gmail.com>
2020-10-12 12:56:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Router from "./Router";
-import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<Router />, document.getElementById("root"));
