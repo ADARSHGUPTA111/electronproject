1eae3dd504195665a6f6e01e477cf9218aa988b9
Anjali Gupta <anjali.gupta@techno-soft.in>
2020-10-19 20:25:45 +0530

    Added Custom Add Icon

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import AddCustomButton from "./AddCustomButton";
 
 export class SideBar extends Component {
   render() {
@@ -9,30 +10,33 @@ export class SideBar extends Component {
     return (
       <>
         <SideMenu>
-          {sideBarData.map((eachLink) => {
-            if(eachLink.label === "Slack" ||  eachLink.label === "Invide Labs"){
+          {sideBarData.map(eachLink => {
+            if (
+              eachLink.label === "Slack" ||
+              eachLink.label === "Invide Labs"
+            ) {
               return (
                 <Label
                   isActiveLink={eachLink.link === activeLink}
                   href=""
-                  onClick={(e) => setActiveLink(e, eachLink.link)}
+                  onClick={e => setActiveLink(e, eachLink.link)}
                 >
-                <img alt="icon" src={eachLink.image} />
+                  <img alt="icon" src={eachLink.image} />
                 </Label>
               );
-            }
-            else{
+            } else {
               return (
                 <Label
                   isActiveLink={eachLink.link === activeLink}
                   href=""
-                  onClick={(e) => setActiveLink(e, eachLink.link)}
+                  onClick={e => setActiveLink(e, eachLink.link)}
                 >
                   <img alt="icon" src={`${eachLink.link}/favicon.ico`} />
                 </Label>
               );
             }
           })}
+          <AddCustomButton />
         </SideMenu>
       </>
     );
@@ -40,35 +44,35 @@ export class SideBar extends Component {
 }
 
 const Label = styled.a`
-display: block;
-padding: 10px;
-transition: ease 2s;
-background-color: ${(props) =>
-  props.isActiveLink ? "rgb(153, 152, 152)" : "none"};
-z-index: ${(props) => (props.isActiveLink ? "-1" : "none")};
-
-&:hover{
-  background-color:darkgrey;
-  cursor: pointer;
-}
-img {
-  width: 35px;
-  height: 35px;
   display: block;
-}
-@media (max-width: 900px) {
   padding: 10px;
-}
+  transition: ease 2s;
+  background-color: ${props =>
+    props.isActiveLink ? "rgb(153, 152, 152)" : "none"};
+  z-index: ${props => (props.isActiveLink ? "-1" : "none")};
+
+  &:hover {
+    background-color: darkgrey;
+    cursor: pointer;
+  }
+  img {
+    width: 35px;
+    height: 35px;
+    display: block;
+  }
+  @media (max-width: 900px) {
+    padding: 10px;
+  }
 `;
 
 const SideMenu = styled.div`
-width: 60px;
-height: 100vh;
-background-color: lightgrey;
-overflow: hidden;
-@media (max-width: 900px) {
-  width: 80px;
-}
+  width: 60px;
+  height: 100vh;
+  background-color: lightgrey;
+  overflow: hidden;
+  @media (max-width: 900px) {
+    width: 80px;
+  }
 `;
 
 export default SideBar;
